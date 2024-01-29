bbfe74b01dd46b1029d17ceec854b379af6ea82e
Elvis Lira <professorelvislira@gmail.com>
2024-01-29 16:47:19 -0300

    Capítulo 1 - Estimativas de variabilidade (average, var e std).

--- Cap1/02_estimativas_de_variabilidade.py
+++ Cap1/02_estimativas_de_variabilidade.py
@@ -1,5 +1,7 @@
 from numpy import array
-from statistics import mean
+from numpy import average
+from numpy import var
+from numpy import std
 from math import sqrt
 from random import randint
 from random import seed
@@ -8,15 +10,15 @@ from random import seed
 seed(100)
 amostra = array([float(randint(1, 10)) for i in range(1, 11)])
 # Média das amostras
-media_amostra = mean(amostra)
+media_amostra = average(amostra)
 # Módulo da diferença de cada valor da amostra pela média 
 desvios_absoluto = abs(amostra - media_amostra)
 # Média dos desvios absolutos
-desvio_absoluto_medio = mean(desvios_absoluto)
+desvio_absoluto_medio = average(desvios_absoluto)
 # Quadrado dos desvios
 desvios_quadraticos = abs(amostra - media_amostra) ** 2
 # Média dos desvios quadráticos
-variancia = mean(desvios_quadraticos)
+variancia = average(desvios_quadraticos)
 # Desvio padrão
 desvio_padrao = sqrt(variancia)
 
@@ -25,5 +27,5 @@ print(f'Média da amostra: {media_amostra:.2f}')
 print(f'Desvios absoluto: {desvios_absoluto}')
 print(f'Desvio absoluto médio: {desvio_absoluto_medio:.2f}')
 print(f'Desvios quadrático: {desvios_quadraticos}')
-print(f'Variância: {variancia:.2f}')
-print(f'Desvio padrão: {desvio_padrao:.2f}')
+print(f'Variância: {variancia:.2f} - {var(amostra):.2f}(função var)')
+print(f'Desvio padrão: {desvio_padrao:.2f} - {std(amostra):.2f}(função std)')
